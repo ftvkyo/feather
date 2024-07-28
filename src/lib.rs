f846977ab72ce7311f282acb2b0c1721db601414
Andrei Stepanenko <ftvkyo@gmail.com>
2024-07-28 13:54:20 +0100

    Use OrbitControl for the camera

--- src/lib.rs
+++ src/lib.rs
@@ -20,44 +20,31 @@ impl App {
     }
 
     pub fn run(self, cpu_mesh: CpuMesh) {
+        let camera_distance = 3.0;
         let camera_target = vec3(0.0, 0.0, 0.0);
         let camera_up = vec3(0.0, 1.0, 0.0);
 
         let mut camera = Camera::new_perspective(
             self.window.viewport(),
-            vec3(3.0, 0.0, 0.0),
+            vec3(camera_distance, 0.0, 0.0),
             camera_target,
             camera_up,
             degrees(60.0),
             0.1,
             10.0,
         );
+        let mut control = OrbitControl::new(
+            camera_target,
+            1.0,
+            10.0,
+        );
 
         let context = self.window.gl();
         let model = Gm::new(Mesh::new(&context, &cpu_mesh), ColorMaterial::default());
 
-        let angle_per_second = 0.1f32; // in radians
-
-        self.window.render_loop(move |frame_input| {
+        self.window.render_loop(move |mut frame_input| {
             camera.set_viewport(frame_input.viewport);
-
-            let secs = frame_input.elapsed_time as f32 / 1_000.0;
-            let angle = angle_per_second * secs;
-            let rotation = Matrix3::from_cols(
-                vec3(angle.cos(), - angle.sin(), 0.0),
-                vec3(angle.sin(), angle.cos(), 0.0),
-                vec3(0.0, 0.0, 1.0),
-            );
-
-            // TODO: Look into OrbitControl
-
-            let camera_position = <Matrix3<_> as Transform<Point3<_>>>::transform_vector(&rotation, camera.position().clone());
-
-            camera.set_view(
-                camera_position,
-                camera_target,
-                camera_up,
-            );
+            control.handle_events(&mut camera, &mut frame_input.events);
 
             frame_input
                 .screen()
