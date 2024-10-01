064a44f3dfd5981e3ed51db39e7d1a5e10e1f8e6
Andrei Stepanenko <ftvkyo@gmail.com>
2024-10-01 21:40:59 +0100

    WIP (boolean 2d): calculate the point of line segment intersection

--- src/geometry/boolean.rs
+++ src/geometry/boolean.rs
@@ -3,17 +3,31 @@ use log::info;
 use super::{Geometry2D, P2};
 
 
-/// Check if 2 line segments intersect
-fn is_intersect(a: &[P2; 2], b: &[P2; 2]) -> bool {
-    // Equation for a `line` to calculate "y" from `x`
-    let f = |line: &[P2; 2], x| line[0].y + (x - line[0].x) * (line[1].y - line[0].y) / (line[1].x - line[0].x);
+/// Check if 2 line segments intersect, return the coordinates of the intersection if yes.
+fn intersection(a: &[P2; 2], b: &[P2; 2]) -> Option<P2> {
+    // FIXME: handle collinear cases
+
+    let slope_a = (a[1].y - a[0].y) / (a[1].x - a[0].x);
+    // Equation for the line `a`
+    let line_a = |x| a[0].y + (x - a[0].x) * slope_a;
+
+    let slope_b = (b[1].y - b[0].y) / (b[1].x - b[0].x);
+    // Equation for the line `b`
+    let line_b = |x| b[0].y + (x - b[0].x) * slope_b;
 
     // Whether points of `b` are on different sides of the line defined by `a`
-    let crosses_a = (f(a, b[0].x) - b[0].y).signum() != (f(a, b[1].x) - b[1].y).signum();
+    let crosses_a = (line_a(b[0].x) - b[0].y).signum() != (line_a(b[1].x) - b[1].y).signum();
     // Whether points of `a` are on different sides of the line defined by `b`
-    let crosses_b = (f(b, a[0].x) - a[0].y).signum() != (f(b, a[1].x) - a[1].y).signum();
+    let crosses_b = (line_b(a[0].x) - a[0].y).signum() != (line_b(a[1].x) - a[1].y).signum();
 
-    return crosses_a && crosses_b;
+    if crosses_a && crosses_b {
+        let intersection_x = (b[0].y - a[0].y - b[0].x * slope_b + a[0].x * slope_a) / (slope_a - slope_b);
+        let intersection_y = line_a(intersection_x);
+
+        Some(P2::new(intersection_x, intersection_y))
+    } else {
+        None
+    }
 }
 
 
@@ -24,7 +38,7 @@ impl Geometry2D {
 
         for ea in &edges_a {
             for eb in &edges_b {
-                if is_intersect(ea, eb) {
+                if let Some(i) = intersection(ea, eb) {
                     info!("Found an intersection:\n -> {:#?}\n -> {:#?}", ea, eb);
                 }
             }
